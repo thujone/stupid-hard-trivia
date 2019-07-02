9f2e1905f0ab3667fe2ea9aa112fa6f383961036
Rich Goldman <rgoldman@presidio.com>
2019-07-02 15:11:19 -0700

    Add index and screenshots

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+export default function About() {
+  return (
+    <Layout>
+      <p>This is a shitty about page</p>
+    </Layout>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+const Index = () => (
+  <Layout>
+    <p>Time for poop</p>
+  </Layout>
+)
+
+export default Index
