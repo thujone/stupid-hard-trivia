1434e250082a3dc854214e438b4760ee13ca8c73
Rich Goldman <rgoldman@presidio.com>
2019-08-18 21:50:45 -0700

    Add social media links

--- components/ad-area.js
+++ components/ad-area.js
@@ -1,15 +1,24 @@
+import React, { Component } from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 
 const Ad = styled.section`
   grid-area: ad;
-  display: none;
 `
 
-const AdArea = () => (
-  <Ad role="contentinfo" id="ad-section">
-    <h4>Ad</h4>
-  </Ad>
-)
+class AdArea extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Ad role="contentinfo" id="ad-section">
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d5a12a287ecab9a"></script>
+        <div className="addthis_inline_share_toolbox"></div>
+      </Ad>
+    )
+  }
+}
 
 export default AdArea
